b9c25e93eed604865392ef2926c40f451f1f311c
Andres <andresruse18@gmail.com>
2024-11-16 16:01:09 +0100

    fourth step

--- src/server_connection.cpp
+++ src/server_connection.cpp
@@ -23,7 +23,7 @@ namespace ServerConnection {
             int client_addr_len = sizeof(client_addr);
             int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 
-            threads.emplace_back(std::thread(handle_connection, client_fd, buffer)).detach();
+            threads.emplace_back(std::thread(handle_connection, client_fd)).detach();
         }
         close(server_fd);
     }
