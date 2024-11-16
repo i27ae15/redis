5079e5e6c42bc53fe076879f4f6f0b5ec1e06b70
Andres <andresruse18@gmail.com>
2024-11-16 12:43:30 +0100

    third step

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,29 +2,33 @@
 #include <cstdlib>
 #include <string>
 #include <cstring>
+#include <stdexcept>
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 
-int main(int argc, char **argv) {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+int createSocket() {
 
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
    std::cerr << "Failed to create server socket\n";
-   return 1;
+   throw std::runtime_error();
   }
+
+  return server_fd;
+}
+
+void checkAddress() {
 
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
     std::cerr << "setsockopt failed\n";
-    return 1;
+    throw std::runtime_error();
   }
 
   struct sockaddr_in server_addr;
@@ -34,27 +38,46 @@ int main(int argc, char **argv) {
 
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
     std::cerr << "Failed to bind to port 6379\n";
-    return 1;
+    throw std::runtime_error();
   }
+}
 
+void checkConnection() {
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
+    throw std::runtime_error();
+  }
+}
+
+int main(int argc, char **argv) {
+  // Flush after every std::cout / std::cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  int server_fd {};
+
+  try:
+    server_fd = createSocket();
+    checkAddress();
+    checkConnection();
+  catch ( const std::runtime_error& e ) {
     return 1;
   }
 
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  std::cout << "Waiting for a client to connect...\n";
+  while (true) {
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  // std::cout << "Logs from your program will appear here!\n";
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
+    std::cout << "Waiting for a client to connect...\n";
 
-  // Uncomment this block to pass the first stage
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // std::cout << "Logs from your program will appear here!\n";
 
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  send(client_fd, "+PONG\r\n", 7, 0);
-  std::cout << "Client connected\n";
+    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    send(client_fd, "+PONG\r\n", 7, 0);
+    std::cout << "Client connected\n";
+  }
 
   close(server_fd);
 
