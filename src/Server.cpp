45f21b364e0796b74963875d37b58760b208d7ed
Andres Melendes <andresruse18@gmail.com>
2024-11-19 13:44:58 +0100

    correctly manage server connection

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,5 +1,6 @@
 #include <server_connection.h>
 #include <config_manager.h>
+#include <db/db_manager.h>
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
@@ -9,9 +10,9 @@ int main(int argc, char **argv) {
   Remus::ConfigManager::initialize(argc, argv);
 
   ServerConnection::ConnectionManager conn;
-  if (!conn.get_connection_status()) return 1;
+  if (!conn.getConnectionStatus()) return 1;
 
-  int server_fd = conn.get_server_fd();
+  int server_fd = conn.getServerFD();
   ServerConnection::listener(server_fd);
 
   return 0;
